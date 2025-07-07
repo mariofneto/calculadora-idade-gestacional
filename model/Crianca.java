3e70863477a1dd5e50b8b456a39204ee1403e3a5
Mario Neto <marioo.netoo3@gmail.com>
2025-07-06 22:03:10 -0300

    classe abstrata Crianca criada

---
+++ model/Crianca.java
@@ -0,0 +1,74 @@
+package model;
+
+import java.util.Date;
+
+public abstract class Crianca {
+
+    private String nome;
+    private Date dataNascimento;
+    private Double pesoEmKg;
+    private Integer idadeGestacionalEmSemanas;
+    private Integer idadeGestacionalEmDias;
+
+    public Crianca() {
+    }
+
+    public Crianca(String nome, Date dataNascimento, Double pesoEmKg, Integer idadeGestacionalEmSemanas, Integer idadeGestacionalEmDias) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.pesoEmKg = pesoEmKg;
+        this.idadeGestacionalEmSemanas = idadeGestacionalEmSemanas;
+        this.idadeGestacionalEmDias = idadeGestacionalEmDias;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Double getPesoEmKg() {
+        return pesoEmKg;
+    }
+
+    public void setPesoEmKg(Double pesoEmKg) {
+        this.pesoEmKg = pesoEmKg;
+    }
+
+    public Integer getIdadeGestacionalEmSemanas() {
+        return idadeGestacionalEmSemanas;
+    }
+
+    public void setIdadeGestacionalEmSemanas(Integer idadeGestacionalEmSemanas) {
+        this.idadeGestacionalEmSemanas = idadeGestacionalEmSemanas;
+    }
+
+    public Integer getIdadeGestacionalEmDias() {
+        return idadeGestacionalEmDias;
+    }
+
+    public void setIdadeGestacionalEmDias(Integer idadeGestacionalEmDias) {
+        this.idadeGestacionalEmDias = idadeGestacionalEmDias;
+    }
+
+    @Override
+    public String toString() {
+        return "Crianca{" +
+                "nome='" + nome + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                ", pesoEmKg=" + pesoEmKg +
+                ", idadeGestacionalEmSemanas=" + idadeGestacionalEmSemanas +
+                ", idadeGestacionalEmDias=" + idadeGestacionalEmDias +
+                '}';
+    }
+}
