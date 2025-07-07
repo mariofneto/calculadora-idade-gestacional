b6bc41c642a1446af66a803f302bd9b879c69d43
Mario Neto <marioo.netoo3@gmail.com>
2025-07-06 22:17:36 -0300

    classes criadas Termo e PreTermo

---
+++ model/PreTermo.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.Date;
+
+public class PreTermo extends Crianca{
+
+    private Integer correcaoIdadeGestacionalSemanas;
+    private Integer correcaoIdadeGestacionalDias;
+
+    public PreTermo() {
+    }
+
+    public PreTermo(String nome, Date dataNascimento, Double pesoEmKg, Integer idadeGestacionalEmSemanas, Integer idadeGestacionalEmDias, Integer correcaoIdadeGestacionalSemanas, Integer correcaoIdadeGestacionalDias) {
+        super(nome, dataNascimento, pesoEmKg, idadeGestacionalEmSemanas, idadeGestacionalEmDias);
+        this.correcaoIdadeGestacionalSemanas = correcaoIdadeGestacionalSemanas;
+        this.correcaoIdadeGestacionalDias = correcaoIdadeGestacionalDias;
+    }
+
+    // preTermo < 37 semanas
+    public String idadeGestacional(){
+        Integer idadeCorretaEmSemanas = 40;
+        Integer diferencaIdadeGestacionalSemanas = idadeCorretaEmSemanas - super.getIdadeGestacionalEmSemanas();
+
+        return "preTermo";
+    }
+}
---
+++ model/Termo.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.util.Date;
+
+public class Termo extends Crianca{
+
+    private Integer diasDeVida;
+
+    public Termo(Integer diasDeVida) {
+        this.diasDeVida = diasDeVida;
+    }
+
+    public Termo(String nome, Date dataNascimento, Double pesoEmKg, Integer idadeGestacionalEmSemanas, Integer idadeGestacionalEmDias, Integer diasDeVida) {
+        super(nome, dataNascimento, pesoEmKg, idadeGestacionalEmSemanas, idadeGestacionalEmDias);
+        this.diasDeVida = diasDeVida;
+    }
+
+    public String diasDeVida(){
+        return String.valueOf(diasDeVida);
+    }
+}
