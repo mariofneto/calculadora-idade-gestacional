67df60091a09117134af4fcc098f891bf63b9728
Mario Neto <marioo.netoo3@gmail.com>
2025-07-20 22:38:24 -0300

    Adicionado o frontend Angular certo

--- dev/src/main/java/com/mario/dev/controller/CriancaController.java
+++ dev/src/main/java/com/mario/dev/controller/CriancaController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class CriancaController {
 
     @Autowired
