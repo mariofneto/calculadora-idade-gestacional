c6c844bb1ca06ed7e9e82dfede286110cabdeab2
Mario Neto <marioo.netoo3@gmail.com>
2025-07-22 21:48:10 -0300

    mds ta foda

--- dev/src/main/java/com/mario/dev/controller/FrontendController.java
+++ dev/src/main/java/com/mario/dev/controller/FrontendController.java
@@ -1,14 +1,15 @@
 package com.mario.dev.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FrontendController {
 
-    @RequestMapping(value = {"/{path:[^\\.]*}", "/**/{path:^(?!api$).*$}"})
-    public String forward() {
+    @GetMapping(value = {
+            "/", "/{path:^(?!api$).*$}", "/**/{path:^(?!api$).*$}"
+    })
+    public String redirect() {
         return "forward:/index.html";
     }
 }
-
